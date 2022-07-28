9f593bebd18266e91cc7faef69b64c5b8f213890
akshit-senpai <chhabra.akshit@gmail.com>
2022-07-28 23:37:00 +0530

    First push

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*function getRes() {
+	ScrHeight = $(window).height();
+	ScrWidth = $(window).width();
+	text = 'width:' + ScrWidth + 'px; height:' + ScrHeight + 'px;';
+	console.log(text);
+	document.getElementById('frame').setAttribute('style', text);
+}
+*/
