e708d79e94e0117853b0429a52e9c9ec08e7ff02
akshit-senpai <chhabra.akshit@gmail.com>
2022-07-29 09:40:21 +0530

    added 10 volumes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,53 @@
-/*function getRes() {
-	ScrHeight = $(window).height();
-	ScrWidth = $(window).width();
-	text = 'width:' + ScrWidth + 'px; height:' + ScrHeight + 'px;';
-	console.log(text);
-	document.getElementById('frame').setAttribute('style', text);
+function init() {
+	shade = document.getElementById('shade');
+	shadeIcon = document.getElementById('shadeIcon');
+	shadeOn = false;
+
+	body = document.getElementById('body');
+	fullscreenIcon = document.getElementById('fullscreenIcon');
+	fullscreenOn = false;
 }
-*/
+
+function toggleShade() {
+	if (shadeOn) {
+		shade.setAttribute('style', 'display:none');
+		shadeIcon.setAttribute('name', 'sunny-outline');
+	}
+	if (!shadeOn) {
+		shade.removeAttribute('style');
+		shadeIcon.setAttribute('name', 'sunny');
+	}
+	shadeOn = !shadeOn;
+}
+
+function toggleFullscreen() {
+	if (fullscreenOn) {
+		document.exitFullscreen();
+		fullscreenIcon.setAttribute('name', 'scan-outline');
+	}
+	if (!fullscreenOn) {
+		body.requestFullscreen();
+		fullscreenIcon.setAttribute('name', 'scan');
+	}
+	fullscreenOn = !fullscreenOn;
+}
+
+function loadHome() {
+	window.location.href = '../index.html';
+}
+
+function getVolume() {
+	url = window.location.href;
+	volume = url.slice(-6, -5);
+	return parseInt(volume);
+}
+
+function loadPrevious() {
+	vol = getVolume();
+	window.location.href = 'volume' + (vol - 1) + '.html';
+}
+
+function loadNext() {
+	vol = getVolume();
+	window.location.href = 'volume' + (vol + 1) + '.html';
+}
